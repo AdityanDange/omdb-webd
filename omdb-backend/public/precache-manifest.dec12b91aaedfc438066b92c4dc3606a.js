b8f9a0fc34aebb8e9955ad19d7a9f1e6e8898934
AdityanDange <dangeadityan@gmail.com>
2020-08-13 15:06:28 +0530

    first commit

---
+++ omdb-backend/public/precache-manifest.dec12b91aaedfc438066b92c4dc3606a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a70b47fb0d82ffcf3d71e70fd78e1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d23344d457df66ea86e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "512489ea785d48d44d8b",
+    "url": "/static/css/main.a5136d75.chunk.css"
+  },
+  {
+    "revision": "3d23344d457df66ea86e",
+    "url": "/static/js/2.229bc380.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.229bc380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "512489ea785d48d44d8b",
+    "url": "/static/js/main.1834f518.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  }
+]);
